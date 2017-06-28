d1a42489da1580dd7e88e412c5d651802be907e3
maestroramesh9808 <maestro.ramesh@gmail.com>
2017-06-28 12:29:58 +0530

    vowel.C

---
+++ vowel.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#inclde<conio.h>
+void main ()
+{
+printf("Enter the character");
+scanf("%c" & c);
+if (c=="a"|| c=="e"|| c=="i"|| c=="o"|| c=="e"|| c=="A"|| c=="E"|| c=="I"|| c=="O"|| c=="U"||)
+{
+printf("%c  IT IS A VOWEL,c);
+}
+else
+{
+printf("%c  IT IS A CONSONANT,c);
+}
+getch();
+return 0;
+}
