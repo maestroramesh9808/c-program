4a90ac6a67230799d6933f440bcceeb4fc97a364
maestroramesh9808 <maestro.ramesh@gmail.com>
2017-06-28 16:11:49 +0530

    natural.C

---
+++ natural.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int i, j, sum = 0;
+scanf("%d",& j);
+ for(i=1; i <= n; i++)
+{
+for(j=1;j<=n;j++)
+sum += i;  // sum = sum+i;
+}
+printf("Sum = %d",sum);
+ return 0;
+}
