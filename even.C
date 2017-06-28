96461fa0dc1da07277bfe7a0bad2e05517c079d9
maestroramesh9808 <maestro.ramesh@gmail.com>
2017-06-28 12:00:20 +0530

    even.C

---
+++ even.C
@@ -0,0 +1,12 @@
+#include(stdio.h)
+#include(conio.h)
+void main()
+{
+printf("Please Enter the Number");
+scanf("%id".&number);
+if (Number %2 = = 0)
+printf("The number is Even");
+else
+printf("The number is odd");
+return 0:
+}
