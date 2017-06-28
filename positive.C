8ebbe2f4fb9a45412ece3d5c483259e2754e1408
maestroramesh9808 <maestro.ramesh@gmail.com>
2017-06-28 11:35:29 +0530

    positive.C

---
+++ positive.C
@@ -0,0 +1,17 @@
+#include(stdio.h)
+#include(conio.h)
+int main()
+{
+printf(Enter the number);
+scanf("%d", & number);
+if (number < = 0.0)
+{
+if (number ==0.0)
+printf("You have enter 0 ");
+else
+printf(" your number is pasitive");
+}
+else
+printf("your number negative");
+return 0:
+}
