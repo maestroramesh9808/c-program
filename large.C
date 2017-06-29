53131c76cd18d068e8e732ae959158132ffa4121
maestroramesh9808 <maestro.ramesh@gmail.com>
2017-06-29 15:19:35 +0530

    large.C

---
+++ large.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+Int a,b,c:
+scanf("%d %d %d", &a, &b, &c);
+if(a>b && a>c)
+printf("Largest number =  a);
+if(b>a && b>c)
+printf("Largest number =  b);
+if(c>a && c>b)
+printf("Largest number = c);
+return 0;
+}
